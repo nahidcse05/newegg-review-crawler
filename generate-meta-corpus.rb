f06a849ae3645bb6796d506507cc0b9d9a4e2604
Chase Geigle <geigle1@illinois.edu>
2013-10-05 20:51:01 -0500

    Generate a corpus over the categories, and make prefix be parameterizable.

--- generate-meta-corpus.rb
+++ generate-meta-corpus.rb
@@ -1,8 +1,21 @@
 require 'json'
+require 'fileutils'
+
+if ARGV.size == 0
+  puts "Usage: ruby generate-meta-corpus.rb prefix"
+  exit 1
+end
 
 all = JSON.parse(File.open("newegg-reviews.json", 'r').read)
-prefix = "/home/sean/projects/meta-data/newegg"
-full_corpus = File.open("#{prefix}/newegg-full-corpus.txt", 'w')
+prefix = ARGV[0]
+senti_corpus = File.open("#{prefix}/senti-full-corpus.txt", 'w')
+category_corpus = File.open("#{prefix}/category-full-corpus.txt", 'w')
+
+def get_file(filename)
+  dir = File.dirname(filename)
+  FileUtils.mkdir_p(dir) unless File.directory?(dir)
+  return File.open(filename, 'w')
+end
 
 i = 0
 for cat, items in all
@@ -11,18 +24,28 @@ for cat, items in all
 
       if rev["Pros"].split.size > 5
         pfilename = "pos/#{cat}-#{item}-pos-#{i}"
-        pout = File.open("#{prefix}/#{pfilename}", 'w')
+        pout = get_file("#{prefix}/#{pfilename}")
         pout << rev["Pros"] << "\n"
         pout.close()
-        full_corpus << "pos #{pfilename}" << "\n"
+        senti_corpus << "pos #{pfilename}" << "\n"
       end
 
       if rev["Cons"].split.size > 5
         nfilename = "neg/#{cat}-#{item}-neg-#{i}"
-        nout = File.open("#{prefix}/#{nfilename}", 'w')
+        nout = get_file("#{prefix}/#{nfilename}")
         nout << rev["Cons"] << "\n"
-        full_corpus << "neg #{nfilename}" << "\n"
+        senti_corpus << "neg #{nfilename}" << "\n"
         nout.close()
+      end
+
+      if rev["Pros"].split.size +
+         rev['Cons'].split.size +
+         rev['Comments'].split.size > 10
+        filename = "#{cat}/#{item}-#{i}"
+        out = get_file("#{prefix}/#{filename}")
+        out << rev['Pros'] << "\n\n" << rev['Cons'] << "\n\n" << rev['Comments'] << "\n"
+        category_corpus << "#{cat} #{filename}" << "\n"
+        out.close()
       end
 
       i += 1
